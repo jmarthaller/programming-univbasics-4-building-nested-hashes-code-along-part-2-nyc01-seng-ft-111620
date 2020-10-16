2cee31e991807461af3cc05a4fa11730e5f05b18
Jacob Marthaller <marthaller.jr@gmail.com>
2020-10-16 14:50:22 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -64,8 +64,9 @@ def add_characters
 
     # epic_tragedy = {:additional_characters => [{:name => "Prince Escalus"},{:name => "Apothecary"}]}
     epic_tragedy[:additional_characters] = [] 
-    epic_tragedy[:additional_characters] << [] 
-    => [{:name => "Prince Escalus"},{:name => "Apothecary"}]}
+    epic_tragedy[:additional_characters] << {:name => "Prince Escalus"}
+    epic_tragedy[:additional_characters] << {:name => "Apothecary"}
+
 
 
 
